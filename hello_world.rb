d56936bfd86b9778e8f0a816837975bb7574cabf
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-03-07 16:08:22 +0100

    add hello_world.rb

---
+++ hello_world.rb
@@ -0,0 +1,5 @@
+names = ['Rickard', 'Mike', 'Leonard']
+
+names.each do |name|
+  puts "Hello #{name}"
+end
---
+++ my_group.rb
@@ -0,0 +1,9 @@
+my_group = []
+
+person_1 = {name: 'Rickard', gender: 'male', age: 45}
+person_2 = {name: 'Mike', gender: 'male', age: 45}
+person_3 = {name: 'Leonard', gender: 'male', age: 45}
+
+my_group.push person_1
+my_group.push person_2
+my_group.push person_3
